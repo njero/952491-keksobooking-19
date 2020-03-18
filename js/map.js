7c93a20e1d77501372b7c8bf86f0e6716dc9bcc0
Alina <alina.alafer@gmail.com>
2020-03-18 11:37:20 +0700

    Drag&Drop

--- js/map.js
+++ js/map.js
@@ -2,10 +2,26 @@
 
 // Модуль карты
 (function () {
+
+  var TAIL_HEIGHT = 16;
+
+  var DEFAULT_MAIN_PIN_COORDS = {
+    x: 600,
+    y: 375
+  };
+
   var PinSize = {
     WIDTH: 65,
     HEIGHT: 65,
   };
+
+  var DragBorder = {
+    LEFT: 0,
+    RIGHT: 1200,
+    TOP: 130 + TAIL_HEIGHT,
+    BOTTOM: 630
+  };
+
 
   var MAX_OFFERS = 8;
   var similarOffers = [];
@@ -23,7 +39,7 @@
 
   window.load(onLoadSuccess, onLoadError);
 
-
+  // Обработчики нажатия на метки (передаем саму метку и данные объявления)
   var onPinClick = function (item, data) {
     item.addEventListener('click', function () {
       window.card.open(data);
@@ -36,6 +52,7 @@
     });
   };
 
+  // Генерация меток
   var createNewPins = function () {
     var fragment = document.createDocumentFragment();
     var similarPinTemplate = document.querySelector('#pin').content
@@ -53,14 +70,72 @@
     return fragment;
   };
 
+  // Отображение меток
   var showNewPins = function () {
     var newPin = createNewPins();
     var similarListElement = document.querySelector('.map__pins');
     similarListElement.appendChild(newPin);
   };
 
+  // -------- Перемещение главной метки ----------
+
+  pinMain.addEventListener('mousedown', function (evt) {
+    evt.preventDefault();
+
+    var startCoords = {
+      x: evt.clientX,
+      y: evt.clientY
+    };
+
+    var onMouseMove = function (moveEvt) {
+      moveEvt.preventDefault();
+
+      var shift = {
+        x: startCoords.x - moveEvt.clientX,
+        y: startCoords.y - moveEvt.clientY
+      };
+
+      startCoords = {
+        x: moveEvt.clientX,
+        y: moveEvt.clientY
+      };
+
+      var mainPinPosition = {
+        x: pinMain.offsetLeft - shift.x,
+        y: pinMain.offsetTop - shift.y
+      };
+
+      var pinTailCoords = {
+        x: mainPinPosition.x + Math.ceil(PinSize.WIDTH / 2),
+        y: mainPinPosition.y + PinSize.HEIGHT + TAIL_HEIGHT
+      };
+
+      if (pinTailCoords.x >= DragBorder.LEFT && pinTailCoords.x <= DragBorder.RIGHT) {
+        pinMain.style.left = mainPinPosition.x + 'px';
+      }
+      if (pinTailCoords.y >= (DragBorder.TOP)
+        && pinTailCoords.y <= DragBorder.BOTTOM) {
+        pinMain.style.top = mainPinPosition.y + 'px';
+      }
+
+      window.form.setAddress(pinTailCoords);
+
+    };
+
+    var onMouseUp = function (upEvt) {
+      upEvt.preventDefault();
+
+      document.removeEventListener('mousemove', onMouseMove);
+      document.removeEventListener('mouseup', onMouseUp);
+    };
+
+    document.addEventListener('mousemove', onMouseMove);
+    document.addEventListener('mouseup', onMouseUp);
+  });
+
 
   window.map = {
+    DEFAULT_MAIN_PIN_COORDS: DEFAULT_MAIN_PIN_COORDS,
     PinSize: PinSize,
     element: map,
     pinMain: pinMain,
